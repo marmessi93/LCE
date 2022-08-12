af26d80d035918688c759ff6b74ce18080df14b3
LocalCascadeEnsemble <kfauvel.lce@gmail.com>
2022-08-12 12:01:08 +0200

    -

---
+++ examples/lceclassifier_iris_cv.py
@@ -0,0 +1,23 @@
+"""
+======================================================================
+2. LCEClassifier on Iris dataset with scikit-learn cross validation score
+======================================================================
+
+An example of :class:`lce.LCEClassifier`
+"""
+
+from lce import LCEClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import cross_val_score, train_test_split
+
+# Load data
+data = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, random_state=0)
+
+# Set LCEClassifier with default parameters
+clf = LCEClassifier(n_jobs=-1, random_state=0)
+
+# Compute cross-validation scores
+cv_scores = cross_val_score(clf, X_train, y_train, cv=3)
+cv_scores = [round(elem*100, 1) for elem in cv_scores.tolist()]
+print("Cross-validation scores on train set: ", cv_scores)
---
+++ examples/lceregressor_diabetes_gridsearchcv.py
@@ -0,0 +1,31 @@
+"""
+================================================================================
+5. LCERegressor on Diabetes dataset with scikit-learn hyperparameter grid search
+================================================================================
+
+An example of :class:`lce.LCERegressor`
+"""
+
+from lce import LCERegressor
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+
+# Load data and generate a train/test split
+data = load_diabetes()
+X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, random_state=0)
+
+# Build LCERegressor with default parameters
+reg = LCERegressor(n_jobs=-1, random_state=0)
+
+# Define parameter ranges for grid search
+params = {'n_estimators': list(range(10, 51, 20)),
+          'max_depth': list(range(0, 3, 1))}
+
+# Run scikit learn grid search 
+grid_cv = GridSearchCV(reg, param_grid=params, cv=3, n_jobs=-1)
+grid_cv.fit(X_train, y_train)
+
+# Print best configuration
+print("Best n_estimator: ", grid_cv.best_params_['n_estimators'],
+      ", best max_depth: ", grid_cv.best_params_['max_depth'])
