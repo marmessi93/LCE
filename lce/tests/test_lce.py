479b34afa8f1aee976750e3c0932535c41f5155b
LocalCascadeEnsemble <kfauvel.lce@gmail.com>
2022-10-03 17:46:37 +0200

    -

--- lce/tests/test_lce.py
+++ lce/tests/test_lce.py
@@ -1,5 +1,10 @@
 import numpy as np
-from sklearn.datasets import load_iris, load_diabetes
+from sklearn.datasets import (
+    load_breast_cancer,
+    load_diabetes,
+    load_iris,
+    make_regression,
+)
 from sklearn.utils.estimator_checks import check_estimator
 import unittest
 import warnings
@@ -44,6 +49,21 @@ class Test(unittest.TestCase):
         with warnings.catch_warnings():
             LCEClassifier(verbose=1).fit(data.data, data.target)
 
+    def test_classifier(self):
+        # Load Breast Cancer dataset
+        data = load_breast_cancer()
+
+        # Fit and predict
+        with warnings.catch_warnings():
+            clf = LCEClassifier(
+                n_estimators=3,
+                max_depth=50,
+                min_samples_leaf=1,
+                random_state=0,
+                verbose=1,
+            ).fit(data.data, data.target)
+            clf.predict(data.data)
+
     def test_classifier_missing(self):
         # Load Iris dataset
         data = load_iris()
@@ -53,48 +73,60 @@ class Test(unittest.TestCase):
         m = 0.02
         for j in range(0, data.data.shape[1]):
             sub = np.random.choice(data.data.shape[0], int(data.data.shape[0] * m))
-            data.data[sub, j] = np.nan
+            temp = data.data
+            temp[sub, j] = np.nan
 
         with warnings.catch_warnings():
-            LCEClassifier(max_depth=50, min_samples_leaf=1, random_state=0).fit(
-                data.data, data.target
-            )
+            clf = LCEClassifier(
+                n_estimators=3,
+                max_depth=50,
+                min_samples_leaf=1,
+                random_state=0,
+                verbose=1,
+            ).fit(temp, data.target)
+            clf.predict(temp)
 
         # Input 20% of missing values per variable
         np.random.seed(0)
         m = 0.2
         for j in range(0, data.data.shape[1]):
             sub = np.random.choice(data.data.shape[0], int(data.data.shape[0] * m))
-            data.data[sub, j] = np.nan
+            temp = data.data
+            temp[sub, j] = np.nan
 
         with warnings.catch_warnings():
-            LCEClassifier(max_depth=50, min_samples_leaf=1, random_state=0).fit(
-                data.data, data.target
-            )
+            clf = LCEClassifier(
+                n_estimators=3, max_depth=50, min_samples_leaf=1, random_state=0
+            ).fit(temp, data.target)
+            clf.predict(temp)
 
         # Input 60% of missing values per variable
         np.random.seed(0)
         m = 0.6
         for j in range(0, data.data.shape[1]):
             sub = np.random.choice(data.data.shape[0], int(data.data.shape[0] * m))
-            data.data[sub, j] = np.nan
+            temp = data.data
+            temp[sub, j] = np.nan
 
         with warnings.catch_warnings():
-            LCEClassifier(max_depth=50, min_samples_leaf=1, random_state=0).fit(
-                data.data, data.target
-            )
+            clf = LCEClassifier(
+                n_estimators=3, max_depth=50, min_samples_leaf=1, random_state=0
+            ).fit(temp, data.target)
+            clf.predict(temp)
 
         # Input 100% of missing values per variable
         np.random.seed(0)
         m = 1.0
         for j in range(0, data.data.shape[1]):
             sub = np.random.choice(data.data.shape[0], int(data.data.shape[0] * m))
-            data.data[sub, j] = np.nan
+            temp = data.data
+            temp[sub, j] = np.nan
 
         with warnings.catch_warnings():
-            LCEClassifier(max_depth=50, min_samples_leaf=1, random_state=0).fit(
-                data.data, data.target
-            )
+            clf = LCEClassifier(
+                n_estimators=3, max_depth=50, min_samples_leaf=1, random_state=0
+            ).fit(temp, data.target)
+            clf.predict(temp)
 
     def test_classifier_sklearn_estimator(self):
         # scikit-learn check estimator
@@ -134,6 +166,23 @@ class Test(unittest.TestCase):
         with warnings.catch_warnings():
             LCERegressor(verbose=1).fit(data.data, data.target)
 
+    def test_regressor(self):
+        # Load dataset
+        n_samples, n_features = 100, 20
+        rng = np.random.RandomState(0)
+        X, y = make_regression(n_samples, n_features, random_state=rng)
+
+        # Fit and predict
+        with warnings.catch_warnings():
+            reg = LCERegressor(
+                n_estimators=3,
+                max_depth=50,
+                min_samples_leaf=1,
+                random_state=0,
+                verbose=1,
+            ).fit(X, y)
+            reg.predict(X)
+
     def test_regressor_missing(self):
         # Load Diabetes dataset
         data = load_diabetes()
@@ -147,45 +196,56 @@ class Test(unittest.TestCase):
             temp[sub, j] = np.nan
 
         with warnings.catch_warnings():
-            LCERegressor(max_depth=50, min_samples_leaf=1, random_state=0).fit(
-                temp, data.target
-            )
+            reg = LCERegressor(
+                n_estimators=3,
+                max_depth=50,
+                min_samples_leaf=1,
+                random_state=0,
+                verbose=1,
+            ).fit(temp, data.target)
+            reg.predict(temp)
 
         # Input 20% of missing values per variable
         np.random.seed(0)
         m = 0.2
         for j in range(0, data.data.shape[1]):
             sub = np.random.choice(data.data.shape[0], int(data.data.shape[0] * m))
-            data.data[sub, j] = np.nan
+            temp = data.data
+            temp[sub, j] = np.nan
 
         with warnings.catch_warnings():
-            LCERegressor(max_depth=50, min_samples_leaf=1, random_state=0).fit(
-                data.data, data.target
-            )
+            reg = LCERegressor(
+                n_estimators=3, max_depth=50, min_samples_leaf=1, random_state=0
+            ).fit(temp, data.target)
+            reg.predict(temp)
 
         # Input 60% of missing values per variable
         np.random.seed(0)
         m = 0.6
         for j in range(0, data.data.shape[1]):
             sub = np.random.choice(data.data.shape[0], int(data.data.shape[0] * m))
-            data.data[sub, j] = np.nan
+            temp = data.data
+            temp[sub, j] = np.nan
 
         with warnings.catch_warnings():
-            LCERegressor(max_depth=50, min_samples_leaf=1, random_state=0).fit(
-                data.data, data.target
-            )
+            reg = LCERegressor(
+                n_estimators=3, max_depth=50, min_samples_leaf=1, random_state=0
+            ).fit(temp, data.target)
+            reg.predict(temp)
 
         # Input 100% of missing values per variable
         np.random.seed(0)
         m = 1.0
         for j in range(0, data.data.shape[1]):
             sub = np.random.choice(data.data.shape[0], int(data.data.shape[0] * m))
-            data.data[sub, j] = np.nan
+            temp = data.data
+            temp[sub, j] = np.nan
 
         with warnings.catch_warnings():
-            LCERegressor(max_depth=50, min_samples_leaf=1, random_state=0).fit(
-                data.data, data.target
-            )
+            reg = LCERegressor(
+                n_estimators=3, max_depth=50, min_samples_leaf=1, random_state=0
+            ).fit(temp, data.target)
+            reg.predict(temp)
 
     def test_regressor_sklearn_estimator(self):
         # scikit-learn check estimator
